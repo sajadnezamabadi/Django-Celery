b3d610a7a30ec5ed903d6e908b57a5516c7e5d2e
sajad nezam abadi <sajad.nezamabadi@dgshahr.com>
2025-03-31 02:59:40 +0330

    feat: add queu and fix DockerCompose file

--- dcelery/cworker/tasks.py
+++ dcelery/cworker/tasks.py
@@ -1,5 +1,25 @@
 from celery import shared_task
+from time import sleep
+@shared_task
+def task1(queue="celery"):
+    sleep(3)
+    print("this is celery")
+    return
 
 @shared_task
-def sharedtask():
-    pass
+def task2(queue="celery1"):
+    sleep(3)
+    print("this is celery1")
+    return
+
+@shared_task
+def task3(queue="celery2"):
+    sleep(3)
+    print("this is celery2")
+    return
+
+@shared_task
+def task4(queue="celery3"):
+    sleep(4)
+    print("this is celery3")
+    return
---
+++ dcelery/dcelery/__init__.py
@@ -0,0 +1,3 @@
+from dcelery.celery import app as celery_app
+
+__all__ = ("celery_app",)
